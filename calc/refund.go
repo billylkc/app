dff9df1fddfb105e91bca23b95f5f07f6752f73a
Billy Lam <0chun0@gmail.com>
2021-05-04 06:38:50 +0800

    w/e

--- calc/refund.go
+++ calc/refund.go
@@ -190,5 +190,85 @@ ORDER BY DATE DESC
 
 func GetMonthlyRefund(start, end string) ([]RefundRecord, error) {
 	var records []RefundRecord
+	// handle stupid date, add one day before query
+	t, err := time.Parse("2006-01-02", end)
+	if err != nil {
+		return records, err
+	}
+
+	end = t.AddDate(0, 0, 1).Format("2006-01-02")
+	db, err := database.GetConnection()
+	if err != nil {
+		return records, err
+	}
+
+	queryF := `
+SELECT
+    DATE,
+    COUNT(1) as COUNT,
+    SUM(refund_amount) as TOTAL
+FROM
+    (SELECT
+        %s,
+        op.refund_amount
+    FROM order_product op
+INNER JOIN %s o
+ON (o.order_id = op.order_id)
+INNER JOIN order_delivery od ON (od.delivery_sn = o.delivery_sn)
+    WHERE
+(od.created_time >= '%s') AND
+(od.created_time <= '%s') AND
+(op.refund_amount > 0) AND (od.is_active = 1)
+    ORDER BY o.order_id desc
+    ) as oop
+GROUP BY
+    DATE
+ORDER BY DATE DESC
+    `
+	query := fmt.Sprintf(queryF,
+		"CAST(DATE_FORMAT(od.created_time,'%Y-%m-01') as DATE) as DATE",
+		"`order`",
+		start,
+		end,
+	)
+
+	results, err := db.Query(query)
+	defer results.Close()
+	if err != nil {
+		return records, errors.Wrap(err, "cant execute query")
+	}
+
+	var md map[string]bool // List of days with records
+	md = make(map[string]bool)
+	for results.Next() {
+		var rec RefundRecord
+		err = results.Scan(&rec.Date, &rec.Count, &rec.Total)
+		if err != nil {
+			return records, err
+		}
+		records = append(records, rec)
+		md[rec.Date.Format("2006-01-02")] = true
+	}
+
+	// Fill dates with empty records
+	ss, err := util.GenerateDate(start, end, "m")
+	if err != nil {
+		return records, err
+	}
+	for _, s := range ss {
+		if _, ok := md[s]; !ok {
+			tt := now.MustParse(s)
+			r := RefundRecord{
+				Date: tt,
+			}
+			records = append(records, r)
+		}
+
+	}
+	// Sort the stupid slice, desc
+	sort.Slice(records, func(i, j int) bool {
+		return records[j].Date.Before(records[i].Date)
+	})
+
 	return records, nil
 }
